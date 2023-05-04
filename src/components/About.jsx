41b6808d0da3c62f947bac002495185403f547de
ab-kiran-18 <18abkiran5o5@gmail.com>
2023-05-04 13:17:44 +0530

    feat(Text Analyser): :sparkles: Application created

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function About(props) {
+    return (
+        <div className={`container my-4 mx-auto text-${props.mode==='dark'? 'light': 'dark'}`}>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim dolor aperiam minus pariatur, est maiores
+                tempore. Sapiente, unde deleniti commodi aperiam facere sed voluptatem in exercitationem veritatis fugiat
+                laboriosam animi consectetur dolorem id sit aspernatur et. Rerum odit in aut nihil voluptate consectetur
+                reprehenderit autem quas quis adipisci. Iure minima sint voluptatem suscipit officia tenetur dolor? Dicta
+                accusantium debitis, ex, doloremque repudiandae, modi vel vitae maxime eligendi quia ipsam sequi adipisci!
+                Quis unde placeat numquam eaque laudantium ipsam voluptatum eligendi quaerat temporibus odit perspiciatis
+                quos deserunt esse quisquam, consequuntur laboriosam, alias autem similique eos omnis? Corporis dolores
+                quisquam blanditiis provident.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, ab? Eveniet tempore dolorum, asperiores
+                officiis omnis doloribus dolor deleniti ut! Hic voluptates eos accusantium omnis nihil, fugiat quod eligendi
+                dicta molestiae. Amet ducimus inventore distinctio nihil odit alias veritatis vel hic id optio quis est, fugia
+                t cumque magnam ipsam nobis ut expedita soluta ratione ex praesentium pariatur accusantium fugit consectetur?
+                Hic cupiditate ipsum, placeat deleniti adipisci minus commodi odio numquam nobis? Nostrum, iste maiores. Quam
+                sapiente impedit totam reiciendis eos magni, nostrum rem quae, sit delectus consequuntur dignissimos facere
+                mollitia voluptatem iste. Reprehenderit, praesentium officia dignissimos minus eaque suscipit obcaecati vero,
+                similique nam eveniet quaerat cupiditate. Pariatur, in hic numquam vel porro, animi dolore repudiandae molestias,
+                obcaecati temporibus mollitia eveniet dignissimos eum error officia ullam aut earum dolorem! Nisi explicabo nulla
+                natus molestiae ipsum, vitae voluptate minus non unde id architecto temporibus, obcaecati quia, voluptatibus
+                saepe itaque? Atque distinctio, error est eius libero, ratione dolores facilis repellendus optio vitae inventore.
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Textbar.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { nodeModuleNameResolver } from 'typescript';
+
+
+export default function Textbar(props) {
+    const [text, SetText] = useState('');
+    const [copy, SetCopy] = useState('copy')
+
+    const handleUpperCase = () => {
+        let newText = text.toUpperCase();
+        SetText(newText);
+    };
+
+    const handleLowerCase = () => {
+        let newText = text.toLowerCase();
+        SetText(newText);
+    };
+
+    const handleClearCase = () => {
+        SetText('');
+    };
+
+    const handleExtraSpace = () => {
+        let newText = text.split(/[ ]+/);
+        SetText(newText.join(" "));
+    };
+    
+    const handleCopyCase = () => {
+        let element = document.getElementById("previewBox");
+        let newText = element.innerText;
+        if (newText === "enter something to preview here") {
+            alert("there is nothing to copy");
+        } else {
+            SetCopy("Copied!");
+            setTimeout(() => {
+                SetCopy("copy");
+            },2000);
+        }
+        navigator.clipboard.writeText();
+    };
+
+    const handleOnChange = (event) => {
+        SetText(event.target.value);
+    };
+
+    return(
+        <>
+            <h2 className={`text-${props.mode === 'light' ? 'dark' : 'light'}`}><b>{ props.heading }</b></h2>
+            <div className="mb-6" >
+                <textarea
+                    className={"form-control"}
+                    onChange={handleOnChange}
+                    value={text}
+                    placeholder='Enter Text Here'
+                    id="myBox"
+                    rows="8"
+                    style={
+                        {
+                            backgroundColor: props.mode === 'light' ? "rgb(22 25 29 / 10%)": "rgb(237 251 255 / 10%)",
+                            color: props.mode === 'dark' ? 'white' : 'black',
+                        }}
+                >
+                </textarea>
+            </div>
+            <div className="container mx-auto my-4">
+                <div className="row row-cols-1 gap-2">
+                    <button onClick={ handleUpperCase } className="col-sm btn btn-primary">convert to UpperCase</button>
+                    <button onClick={ handleLowerCase } className="col-sm btn btn-primary">convert to LowerCase</button>
+                    <button onClick={handleExtraSpace} className="col-sm btn btn-primary">Remove Extra Spaces</button>
+                    <button onClick={handleClearCase} className="col-sm btn btn-danger">Clear Text</button>
+                </div>
+            </div>
+            <div className={`container text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                <h3> <b>Text Summary</b></h3>
+                <p> {text === "" ? 0: text.split(' ').length } words and {text.length} characters.</p>
+                <p> {0.008 * (text === "" ? 0 : text.split(' ').length).toPrecision(2)} mins read.</p>
+                <p> { text === "" ? 0: text.split('.').length } sentences</p>
+            </div>
+            <div className="container" style={{
+                padding: 15,
+                backgroundColor: props.mode === 'light' ? "rgb(22 25 29 / 10%)": "rgb(237 251 255 / 10%)",
+                color: props.mode === 'light' ? 'black' : 'white'
+            }}>
+                <span className='h3'>Text Preview</span>
+                <span onClick={handleCopyCase} className="btn btn-primary" style={{
+                    float: 'right',
+                    backgroundColor: copy === "Copied!" ? 'green' : 'blue',
+                }}>{copy}</span>
+                <span></span>
+                <p className='mt-2' id="previewBox">{ text.length>0 ? text: "enter something to preview here" }</p>
+            </div>
+        </>
+    )
+}
